d411156b433792aeb5dcc9523cc93937089e2aba
50709200279 <gregormikk@gmail.com>
2024-11-08 11:04:26 +0200

    commit initial

--- KooliProjekt/Data/Services.cs
+++ KooliProjekt/Data/Services.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string transportation { get; set; }
-        public decimal? PanelProduction { get; set; }
+        public decimal PanelProduction { get; set; }
         public string montage { get; set; }
     }
 }
