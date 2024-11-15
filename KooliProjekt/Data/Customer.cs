fc6ba55ca0b6886771b32f532b91f9f98eb58e9d
50709200279 <gregormikk@gmail.com>
2024-11-15 11:30:04 +0200

    uus

--- KooliProjekt/Data/Customer.cs
+++ KooliProjekt/Data/Customer.cs
@@ -1,16 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace KooliProjekt.Data
 {
     public class Customer
     {
         public int Id { get; set; }
+
+        [StringLength(55)]
+        [Required]
         public string Name { get; set; }
+
+
+        [Required]
+        public DateTime Date { get; set; } 
+
         [Required]
         [StringLength(55)]
-        public DateTime date { get; set; }
-        [Required]
-        public string contact { get; set; }
-
+        public string Contact { get; set; } 
     }
 }
